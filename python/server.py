2a89dd5d4a90f0ccc78ef3673d778041ba62f5d7
root <root@localhost.(none)>
2015-05-10 13:52:54 -0400

    v1

---
+++ python/server.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*- 
+import torndb,re
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import sys,os
+import tornado.autoreload
+import time
+import salt.client
+
+from tornado.options import define, options, parse_command_line
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+define("port", default=80, help="run on the given port", type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+	settings = {                                                              
+     "static_path": os.path.join(os.path.dirname(__file__), "../static"),                         
+     "template_path" : os.path.join(os.path.dirname(__file__), "../template"),                    
+     "login_url": "/login/",                                                 
+     "debug" : True,                                                      
+     "cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",                          
+	#"xsrf_cookies":True,                                                  
+	}                                                                   
+                                                                    
+	handlers =[                                                
+    		(r"/", index),                                                       
+    		(r"/master_get/", master_get),                                                       
+    		(r"/slave_1_ip/", slave_1_ip),                                                       
+     ]                                                             
+
+     	tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+     return self.application.db
+    def get_current_user(self):
+     return self.get_secure_cookie("user")
+
+class index(BaseHandler):
+    def get(self):
+     	self.render("index.html")
+
+class master_get(BaseHandler):                                                                                         
+    def post(self):                                                                                                 
+      m_ip=self.get_argument("m_ip") 
+      m_port= self.get_argument("m_port") 
+      client = salt.client.LocalClient()
+      ret = client.cmd(m_ip.encode('utf-8'), 'cmd.run', ['ls -l /data/server/redis/etc/ | awk \'/conf/{print $9}\' | tr "\n" ","'])
+      filename=''.join(ret.values()).split(",")[:-1]
+      for i in filename: 
+	if re.search(m_port.encode('utf-8'),i):
+	   file_path='cat /data/server/redis/etc/%s'%i
+	   config_file = client.cmd(m_ip.encode('utf-8'), 'cmd.run', [file_path])
+	   data= ''.join(config_file.values()).split("\n")
+      self.write("%s"%data)
+
+class slave_1_ip(BaseHandler):
+    def post(self):
+      s_1_ip=self.get_argument("s_1_ip")	
+      client = salt.client.LocalClient()
+      ret = client.cmd(s_1_ip.encode('utf-8'), 'cmd.run', ['ls -l /data/server/redis/etc/ | awk \'/conf/{print $9}\' | tr "\n" ","'])
+      filename=''.join(ret.values()).split(",")[:-1]
+      self.write(str(filename))
+      print filename
+
+def main():
+    parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ static/post.js
@@ -0,0 +1,102 @@
+$(document).ready(function () {
+      $("#setting_btn1").click(function () {
+                if ($("#master_ip").val() == "" || $("#master_port").val() == "") {
+                    alert("请输入MASTER的资料");
+                } else {
+                    $.post("/master_get/",
+                            {
+                                m_ip: $("#master_ip").val(),
+                                m_port: $("#master_port").val()
+                            },
+                            function ( data ) {
+				//data = eval(""+data+"")
+				data=eval(data)
+                                var innerHtml = '';
+                                for (var i = 0; i < data.length; i++) {
+                                    innerHtml = innerHtml + data[i] + '\r\n';
+                                }
+                                $('#result').val(innerHtml);
+                            }
+                    );
+                }
+        });
+
+
+///////////////////////////////////////////////////////////////////////
+      $("#setting_btn2").click(function () {
+                if ($("#slave_1_ip").val() == "" ) {
+                    alert("请输入SLAVE-1的IP地址");
+                } else {
+                    $.post("/slave_1_ip/",
+                            {
+                                s_1_ip: $("#slave_1_ip").val(),
+                            },
+                            function ( data ) {
+				data=eval(data)
+                                var innerHtml = '';
+                                for (var i = 0; i < data.length; i++) {
+                                    innerHtml = innerHtml + data[i] + '\n';
+                                }
+				$("#setting_btn2").attr("title",innerHtml);
+				$("[data-toggle='tooltip']").tooltip();
+                            }
+                    );
+                }
+        });
+
+      $("#setting_btn3").click(function () {
+                if ($("#result").val() == "" ) {
+                    alert("请先获取MASTER的配置文件");
+                } else {
+			$("#s1_config_name").val($("#result").val()) ;
+	
+		}
+        });
+//////////////////////////////////////////////////////////////////////////
+      $("#setting_btn4").click(function () {
+                if ($("#slave_2_ip").val() == "" ) {
+                    alert("请输入SLAVE-1的IP地址");
+                } else {
+                    $.post("/slave_1_ip/",
+                            {
+                                s_1_ip: $("#slave_2_ip").val(),
+                            },
+                            function ( data ) {
+                                data=eval(data)
+                                var innerHtml = '';
+                                for (var i = 0; i < data.length; i++) {
+                                    innerHtml = innerHtml + data[i] + '\n';
+                                }
+                                $("#setting_btn4").attr("title",innerHtml);
+                                $("[data-toggle='tooltip1']").tooltip();
+                            }
+                    );
+                }
+        });
+
+      $("#setting_btn5").click(function () {
+                if ($("#s1_config_name").val() == "" ) {
+                    alert("请先配置SLAVE-1上的配置文件");
+                } else if ($("#s1_config_name").val()==$("#result").val()){
+			alert("请修改SLAVE-1的配置文件中的"+'\n'+"1.端口"+'\n'+"2.PID文件"+'\n'+"3.log文件"+'\n'+"4.rdb文件的名称");
+		
+		}else {
+                        $("#s2_config_name").val($("#s1_config_name").val()) ;
+
+                }
+        });
+
+//////////////////////////////////////////////////////////////////////////
+      $("#second_step").click(function () {
+		 $("#m1").text($("#master_ip").val());
+		 $("#p1").text($("#master_port").val());
+		 $("#s1").text($("#slave_1_ip").val());
+		 $("#c1").text($("#config_s1").val());
+		 $("#s2").text($("#slave_2_ip").val());
+		 $("#c2").text($("#config_s2").val());
+        });
+
+
+
+});
+
